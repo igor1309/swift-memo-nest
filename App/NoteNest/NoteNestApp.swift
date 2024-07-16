f18d2d4e010f18b3a9c426193c8e4f3f1eb26d44
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-07-16 08:56:28 +0300

    add app target

---
+++ App/NoteNest/NoteNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteNestApp.swift
+//  NoteNest
+//
+//  Created by Igor Malyarov on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct NoteNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
