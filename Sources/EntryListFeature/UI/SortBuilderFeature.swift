47a7ac80b225f8e2f8ca3c8b665e5cc97a51c138
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-07-18 16:07:16 +0300

    WIP: SortBuilderFeature

--- Sources/EntryListFeature/UI/SortBuilderFeature.swift
+++ Sources/EntryListFeature/UI/SortBuilderFeature.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Igor Malyarov on 18.07.2024.
 //
@@ -9,6 +9,7 @@
 import Foundation
 
 struct SortBuilderState<T> {
+    
     var selectedComparators: [KeyPathComparator<T>] = []
     var selectedKeyPath: String? = nil
     var sortOrder: SortOrder = .forward
@@ -17,29 +18,44 @@ struct SortBuilderState<T> {
 }
 
 enum SortBuilderEvent {
+    
     case addComparator
     case deleteComparators(IndexSet)
+    case updateSelectedKeyPath(String?)
+    case updateSortOrder(SortOrder)
 }
 
 final class SortBuilderReducer<T> {
-    func reduce(state: inout SortBuilderState<T>, event: SortBuilderEvent) {
+    
+    func reduce(
+        state: inout SortBuilderState<T>,
+        event: SortBuilderEvent
+    ) {
         switch event {
         case .addComparator:
             addComparator(state: &state)
-        case .deleteComparators(let offsets):
+            
+        case let .deleteComparators(offsets):
             state.selectedComparators.remove(atOffsets: offsets)
+            
+        case let .updateSelectedKeyPath(newValue):
+            state.selectedKeyPath = newValue
+            
+        case let .updateSortOrder(newValue):
+            state.sortOrder = newValue
         }
     }
     
     private func addComparator(state: inout SortBuilderState<T>) {
+        
         if let key = state.selectedKeyPath, let keyPath = state.keyPaths[key] {
             if state.selectedComparators.contains(where: { $0.keyPath == keyPath }) {
                 state.showingAlert = true
             } else {
                 if let keyPath = keyPath as? KeyPath<T, String> {
-                    state.selectedComparators.append(KeyPathComparator(keyPath, order: state.sortOrder))
+                    state.selectedComparators.append(.init(keyPath, order: state.sortOrder))
                 } else if let keyPath = keyPath as? KeyPath<T, Int> {
-                    state.selectedComparators.append(KeyPathComparator(keyPath, order: state.sortOrder))
+                    state.selectedComparators.append(.init(keyPath, order: state.sortOrder))
                 }
                 // Add more types as needed
             }
@@ -51,40 +67,65 @@ final class SortBuilderModel<T>: ObservableObject {
     
     @Published private(set) var state: SortBuilderState<T>
     
-    private let reducer: (inout SortBuilderState<T>, SortBuilderEvent) -> Void
+    private let reduce: (inout SortBuilderState<T>, SortBuilderEvent) -> Void
     
-    init(keyPaths: [String: PartialKeyPath<T>], reducer: @escaping (inout SortBuilderState<T>, SortBuilderEvent) -> Void) {
-        self.reducer = reducer
-        self.state = SortBuilderState(selectedKeyPath: keyPaths.keys.first, keyPaths: keyPaths)
+    init(
+        keyPaths: [String: PartialKeyPath<T>],
+        reduce: @escaping (inout SortBuilderState<T>, SortBuilderEvent) -> Void
+    ) {
+        self.reduce = reduce
+        self.state = .init(selectedKeyPath: keyPaths.keys.first, keyPaths: keyPaths)
     }
     
-    func handleEvent(_ event: SortBuilderEvent) {
-        reducer(&state, event)
+    func event(_ event: SortBuilderEvent) {
+        
+        reduce(&state, event)
     }
 }
 
 import SwiftUI
 
 struct SortBuilderView<T>: View {
+    
     @StateObject private var viewModel: SortBuilderModel<T>
     
     init(model: SortBuilderModel<T>) {
-        _viewModel = StateObject(wrappedValue: model)
+        
+        self._viewModel = .init(wrappedValue: model)
     }
     
     var body: some View {
+        
         VStack {
+            
             HStack {
-                Picker("Select Key Path", selection: $viewModel.state.selectedKeyPath) {
-                    Text("Select a key path").tag(String?.none)
+                
+                Picker(
+                    "Select Key Path",
+                    selection: .init(
+                        get: { viewModel.state.selectedKeyPath },
+                        set: { newValue in
+                            viewModel.event(.updateSelectedKeyPath(newValue))
+                        }
+                    )
+                ) {
+                    Text("Select a key path").tag(Optional<String>.none)
                     ForEach(Array(viewModel.state.keyPaths.keys), id: \.self) { key in
-                        Text(key).tag(String?.some(key))
+                        Text(key).tag(Optional(key))
                     }
                 }
                 .pickerStyle(.menu)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
-                Picker("Order", selection: $viewModel.state.sortOrder) {
+                Picker(
+                    "Order",
+                    selection: .init(
+                        get: { viewModel.state.sortOrder },
+                        set: { newValue in
+                            viewModel.event(.updateSortOrder(newValue))
+                        }
+                    )
+                ) {
                     Text("Ascending").tag(SortOrder.forward)
                     Text("Descending").tag(SortOrder.reverse)
                 }
@@ -93,33 +134,39 @@ struct SortBuilderView<T>: View {
             }
             .padding(.vertical)
             
-            Button("Add Comparator") {
-                viewModel.handleEvent(.addComparator)
-            }
-            .padding(.bottom)
-            .alert(isPresented: $viewModel.state.showingAlert) {
-                Alert(
-                    title: Text("Duplicate Comparator"),
-                    message: Text("A comparator with the selected key path already exists."),
-                    dismissButton: .default(Text("OK"))
-                )
-            }
+#warning("replace alert API with one from UIPrimitives")
+            Button("Add Comparator") { viewModel.event(.addComparator) }
+                .padding(.bottom)
+                .alert(
+                    isPresented: .init(
+                        get: { viewModel.state.showingAlert },
+                        set: { _ in viewModel.event(.updateSelectedKeyPath(nil)) }
+                    )
+                ) {
+                    .init(
+                        title: Text("Duplicate Comparator"),
+                        message: Text("A comparator with the selected key path already exists."),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
             
             List {
+                
                 ForEach(viewModel.state.selectedComparators, id: \.keyPath) { comparator in
+                    
                     if let key = viewModel.state.keyPaths.first(where: { $0.value == comparator.keyPath })?.key {
+                        
                         Text("\(key) - \(comparator.order == .forward ? "Ascending" : "Descending")")
                     }
                 }
-                .onDelete { indices in
-                    viewModel.handleEvent(.deleteComparators(indices))
-                }
+                .onDelete { viewModel.event(.deleteComparators($0)) }
             }
             .listStyle(.plain)
             
             Button("Done") {
+                
                 let sort = Sort(comparators: viewModel.state.selectedComparators)
-                // Do something with the sort object
+#warning("Do something with the sort object")
                 print(sort)
             }
         }
@@ -138,9 +185,15 @@ struct ContentView: View {
         SortBuilderView(
             model: SortBuilderModel<Item>(
                 keyPaths: keyPaths,
-                reducer: reducer.reduce
+                reduce: reducer.reduce
             )
         )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
@@ -153,11 +206,5 @@ struct Item: Identifiable {
     let id = UUID()
     let name: String
     let age: Int
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
 }
 #endif
