68d0f9231ec3e401e862f39e365e5792a7554bb6
Igor Malyarov <malyarov_i_a@forabank.ru>
2024-07-18 16:32:54 +0300

    `SortBuilderView`: extract subviews

--- Sources/SortBuilderFeature/UI/SortBuilderView.swift
+++ Sources/SortBuilderFeature/UI/SortBuilderView.swift
@@ -20,80 +20,112 @@ struct SortBuilderView<T>: View {
         
         VStack {
             
-            HStack {
-                
-                Picker(
-                    "Select Key Path",
-                    selection: .init(
-                        get: { viewModel.state.selectedKeyPath },
-                        set: { newValue in
-                            viewModel.event(.updateSelectedKeyPath(newValue))
-                        }
-                    )
-                ) {
-                    Text("Select a key path").tag(Optional<String>.none)
-                    ForEach(Array(viewModel.state.keyPaths.keys), id: \.self) { key in
-                        Text(key).tag(Optional(key))
-                    }
-                }
-                .pickerStyle(.menu)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Picker(
-                    "Order",
-                    selection: .init(
-                        get: { viewModel.state.sortOrder },
-                        set: { newValue in
-                            viewModel.event(.updateSortOrder(newValue))
-                        }
-                    )
-                ) {
-                    Text("Ascending").tag(SortOrder.forward)
-                    Text("Descending").tag(SortOrder.reverse)
-                }
-                .pickerStyle(.menu)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-            }
-            .padding(.vertical)
+            comparatorView()
+                .padding(.vertical)
             
-#warning("replace alert API with one from UIPrimitives")
-            Button("Add Comparator") { viewModel.event(.addComparator) }
+            addComparatorButton()
                 .padding(.bottom)
-                .disabled(viewModel.state.selectedKeyPath == nil)
-                .alert(
-                    isPresented: .init(
-                        get: { viewModel.state.showingAlert },
-                        set: { _ in viewModel.event(.updateSelectedKeyPath(nil)) }
-                    )
-                ) {
-                    .init(
-                        title: Text("Duplicate Comparator"),
-                        message: Text("A comparator with the selected key path already exists."),
-                        dismissButton: .default(Text("OK"))
-                    )
-                }
-            
-            List {
-                
-                ForEach(viewModel.state.selectedComparators, id: \.keyPath) { comparator in
-                    
-                    if let key = viewModel.state.keyPaths.first(where: { $0.value == comparator.keyPath })?.key {
-                        
-                        Text("\(key) - \(comparator.order == .forward ? "Ascending" : "Descending")")
-                    }
-                }
-                .onDelete { viewModel.event(.deleteComparators($0)) }
-            }
-            .listStyle(.plain)
+
+            comparatorList()
             
             Button("Done") {
                 
-                let sort = Sort(comparators: viewModel.state.selectedComparators)
 #warning("Do something with the sort object")
-                print(sort)
+                print(viewModel.state.sort)
             }
         }
         .padding()
+    }
+}
+
+private extension SortBuilderState {
+    
+    var sort: Sort<T> {
+     
+        return .init(comparators: selectedComparators)
+    }
+}
+
+private extension SortBuilderView {
+    
+    func comparatorView() -> some View {
+        
+        HStack {
+            
+            keyPathPicker()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            sortOrderPicker()
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        .pickerStyle(.menu)
+    }
+    
+    func keyPathPicker() -> some View {
+        
+        Picker(
+            "Select Key Path",
+            selection: .init(
+                get: { viewModel.state.selectedKeyPath },
+                set: { viewModel.event(.updateSelectedKeyPath($0)) }
+            )
+        ) {
+            Text("Select a key path").tag(Optional<String>.none)
+            
+            ForEach(Array(viewModel.state.keyPaths.keys), id: \.self) { key in
+                
+                Text(key).tag(Optional(key))
+            }
+        }
+    }
+    
+    func sortOrderPicker() -> some View {
+        
+        Picker(
+            "Order",
+            selection: .init(
+                get: { viewModel.state.sortOrder },
+                set: { viewModel.event(.updateSortOrder($0)) }
+            )
+        ) {
+            Text("Ascending").tag(SortOrder.forward)
+            Text("Descending").tag(SortOrder.reverse)
+        }
+    }
+    
+#warning("replace alert API with one from UIPrimitives")
+    func addComparatorButton() -> some View {
+        
+        Button("Add Comparator") { viewModel.event(.addComparator) }
+            .disabled(viewModel.state.selectedKeyPath == nil)
+            .alert(
+                isPresented: .init(
+                    get: { viewModel.state.showingAlert },
+                    set: { _ in viewModel.event(.updateSelectedKeyPath(nil)) }
+                )
+            ) {
+                .init(
+                    title: Text("Duplicate Comparator"),
+                    message: Text("A comparator with the selected key path already exists."),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
+    }
+    
+    func comparatorList() -> some View {
+        
+        List {
+            
+            ForEach(viewModel.state.selectedComparators, id: \.keyPath) { comparator in
+                
+                if let key = viewModel.state.keyPaths.first(where: { $0.value == comparator.keyPath })?.key {
+                    
+                    Text("\(key) - \(comparator.order == .forward ? "Ascending" : "Descending")")
+                }
+            }
+            .onDelete { viewModel.event(.deleteComparators($0)) }
+        }
+        .listStyle(.plain)
     }
 }
 
